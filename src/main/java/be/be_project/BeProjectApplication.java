0c80c5ba1f1ea4b90acd9a741a63c8d5c641394c
realcommang <katlelo@naver.com>
2024-05-04 01:50:06 +0900

    [ADD] Dockerfile

--- src/main/java/be/be_project/BeProjectApplication.java
+++ src/main/java/be/be_project/BeProjectApplication.java
@@ -2,8 +2,9 @@ package be.be_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class BeProjectApplication {
 
 	public static void main(String[] args) {
