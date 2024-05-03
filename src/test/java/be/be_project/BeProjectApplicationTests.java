0727afe8a6716f83099eb389348581018605b9e6
leejunnyeop <daffodil8620@naver.com>
2024-05-03 23:16:39 +0900

    init

---
+++ src/test/java/be/be_project/BeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package be.be_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
